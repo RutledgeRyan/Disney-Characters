1cb41b3844b2adab31d1ac356a5c3fbc333c7caa
ownoel <onoel@purdue.edu>
2024-11-13 15:53:33 -0500

    Add files via upload

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -18,7 +18,7 @@ const CharacterModal = ({ data, onClose }) => (
         Princess Information
       </h2>
       <p className="text-lg text-green-700">
-        Tv Shows: {data.tvShows}
+        Link: {data.sourceUrl}
       </p>
       <button
         className="mt-4 bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
